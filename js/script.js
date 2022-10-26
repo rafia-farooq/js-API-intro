03589c89890692cd112a11e7aa31fd23a4812636
r-farooq <rafiafarooq@hotmail.com>
2022-10-26 16:00:21 +0300

    display image and author on button click

--- js/script.js
+++ js/script.js
@@ -3,10 +3,38 @@ const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
+// connect to API
 const getImage = async function(){
-    const response = await fetch("https://picsum.photos/v2/list?limit=10");
-    const image = await response.json();
-    console.log(image);
+    const response = await fetch("https://picsum.photos/v2/list?limit=100");
+    const images = await response.json();
+    console.log(images);
+
+    // select the image
+    randomNumber(images);
+    
 }
 
-getImage();
+// Call function to get result in the console.
+// getImage();
+
+// create a random number
+
+const randomNumber = function(images){
+    const randomIndex = Math.floor(Math.random() * images.length);
+    // console.log(randomIndex);
+    const imageObject = images[randomIndex];
+    // console.log(imageObject);
+    displayImage(imageObject);
+}
+
+const displayImage = function(imageObject){
+    const url = imageObject.download_url;
+    const author = imageObject.author;
+    authorSpan.innerText = author;
+    img.src = url;
+}
+
+button.addEventListener("click", function(){
+    imgDiv.classList.remove("hide");
+    getImage();
+})
