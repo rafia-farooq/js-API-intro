6981b1cef83baaa8c2fae7bb90340015b323df49
r-farooq <rafiafarooq@hotmail.com>
2022-10-26 15:06:40 +0300

    created function for API

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@ const button = document.querySelector("button");
 const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
+
+const getImage = async function(){
+    const response = await fetch("https://picsum.photos/v2/list?limit=10");
+    const image = await response.json();
+    console.log(image);
+}
+
+getImage();
